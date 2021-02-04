a900dec102684ee556b3d165f799ac5b1a44a7d6
Hong Jung hyeon <jhhong0509@naver.com>
2021-02-04 11:57:12 +0900

    [CREATE] basic project setting

---
+++ src/main/java/com/example/ddangui/DdanguiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ddangui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdanguiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdanguiApplication.class, args);
+	}
+
+}
